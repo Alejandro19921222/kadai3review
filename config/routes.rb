0ac17de8fbb69a9400af09f02aeb187de0ce297b
Alejabdro19921222 <niojapon@gmail.com>
2021-01-26 06:05:18 +0000

    [finish]day2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'book/index'
+  get 'book/show'
+  get 'book/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get 'home/about', to: 'homes#about'
+  resources :user, only:[:update, :index, :show, :edit]
+  resources :book, only:[:index, :show, :edit, :create, :update, :destroy]
 end
